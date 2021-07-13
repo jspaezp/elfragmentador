9d58c1cc8f22ad80e9b3a54e5855d5416bfe24da
Jspaezp <jspaezp@gmail.com>
2021-07-13 01:24:36 -0400

    fixed minor bug where run name would not be correct

--- elfragmentador/train.py
+++ elfragmentador/train.py
@@ -100,7 +100,7 @@ def get_callbacks(
         dirpath=".",
         save_last=True,
         mode="min",
-        filename="{complete_run_name}_{v_l:.6f}_{epoch:03d}",
+        filename=complete_run_name + "_{v_l:.6f}_{epoch:03d}",
     )
 
     terminator = pl.callbacks.early_stopping.EarlyStopping(
