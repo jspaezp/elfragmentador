aa80372ccc83eefd87406805ffec36e778bded12
Jspaezp <jspaezp@gmail.com>
2021-08-23 03:05:46 -0400

    major improvement in docstings in annotate

--- elfragmentador/annotate.py
+++ elfragmentador/annotate.py
@@ -16,7 +16,22 @@ from numpy import bool_, float64, ndarray
 from elfragmentador import constants, encoding_decoding
 
 
-def solve_alias(x):
+def _solve_alias(x: str) -> str:
+    """
+    _solve_alias Gets the cannnonical form of an aminoacid
+
+    Args:
+        x (str): An aminoacid (possibly modified) to be queried
+
+    Returns:
+        str: Solved alias
+
+    Examples:
+        >>> _solve_alias("M(ox)")
+        'M[OXIDATION]'
+        >>> _solve_alias("M[+16]")
+        'M[OXIDATION]'
+    """
     try:
         x = x if len(x) == 1 else x[:1] + f"[{constants.MOD_PEPTIDE_ALIASES[x]}]"
     except KeyError:
@@ -31,18 +46,44 @@ def solve_alias(x):
     return x
 
 
-def peptide_parser(p: str, solve_aliases=False) -> Iterator[str]:
+def peptide_parser(p: str, solve_aliases: bool =False) -> Iterator[str]:
     """
-    Parses maxquant formatted peptide strings
+    peptide_parser Parses peptides in a string to an iterable
 
-    Examples
-    ========
-    >>> list(peptide_parser("AAACC"))
-    ['n', 'A', 'A', 'A', 'C', 'C', 'c']
-    >>> list(peptide_parser("AAAM(ox)CC"))
-    ['n', 'A', 'A', 'A', 'M(ox)', 'C', 'C', 'c']
-    >>> list(peptide_parser("AAAM[+16]CC"))
-    ['n', 'A', 'A', 'A', 'M[+16]', 'C', 'C', 'c']
+    Args:
+        p (str): Peptide sequence in a single string
+        solve_aliases (bool, optional): Wether to solve aliases for modifications. Defaults to False.
+
+    Raises:
+        ValueError:
+            Raises an error when the sequence cannot be correcly parsed 
+            (starts with a special chatacter for instance)
+
+    Yields:
+        Iterator[str]: Every element in the peptide sequence
+    """    
+    
+
+    pass
+    """Parses peptide sequences as strings
+    
+
+
+    Args:
+        p (str): Peptide string to be parsed
+        solve_aliases (bool):  (Default value = False)
+
+    Returns:
+
+      
+
+    Examples:
+        >>> list(peptide_parser("AAACC"))
+        ['n', 'A', 'A', 'A', 'C', 'C', 'c']
+        >>> list(peptide_parser("AAAM(ox)CC"))
+        ['n', 'A', 'A', 'A', 'M(ox)', 'C', 'C', 'c']
+        >>> list(peptide_parser("AAAM[+16]CC"))
+        ['n', 'A', 'A', 'A', 'M[+16]', 'C', 'C', 'c']
     """
 
     ANNOTATIONS = "[](){}"
@@ -72,7 +113,7 @@ def peptide_parser(p: str, solve_aliases=False) -> Iterator[str]:
             j = min(nexts)
             offset = i + j + 3
             out = p[i:offset]
-            yield_value = solve_alias(out) if solve_aliases else out
+            yield_value = _solve_alias(out) if solve_aliases else out
             i = offset
         else:
             yield_value = p[i]
@@ -86,6 +127,14 @@ def peptide_parser(p: str, solve_aliases=False) -> Iterator[str]:
 
 
 def mass_diff_encode_seq(seq):
+    """
+
+    Args:
+      seq: 
+
+    Returns:
+
+    """
     iter = peptide_parser(seq, solve_aliases=True)
     iter = encoding_decoding.clip_explicit_terminus(list(iter))
     # For some reason skyline detects T[80] but not T[+80] ...
@@ -96,27 +145,22 @@ def mass_diff_encode_seq(seq):
 
 def canonicalize_seq(seq: str, robust: bool = False) -> str:
     """canonicalize_seq Solves all modification aliases in a sequence.
-
+    
     Given a sequence, converts al supported modification aliases to the
     "canonical" version of them and returns the new version.
 
-    Parameters
-    ----------
-    seq : str
-        Modified peptide sequence, for example "PEPTIDE[+23]TIDE")
-    robust : bool, optional
-        Wether you want error to be silent and return none when they happen, by default False
+    Args:
+      seq(str): Modified peptide sequence, for example "PEPTIDE[+23]TIDE")
+      robust(bool): Wether you want error to be silent and return none when they happen, by default False
 
-    Returns
-    -------
-    str
-        Same sequence as input but with all mod aliases replaced for the primary
-        one in this package
+    Returns:
+      str: Same sequence as input but with all mod aliases replaced for the primary
+      one in this package
 
-    Raises
-    ------
-    e
-        [description]
+    Examples:
+      >>> canonicalize_seq("PEPTM(ox)IDEPINK")
+      'nPEPTM[OXIDATION]IDEPINKc'
+
     """
     try:
         out = "".join(peptide_parser(seq, solve_aliases=True))
@@ -130,13 +174,11 @@ def canonicalize_seq(seq: str, robust: bool = False) -> str:
 
 
 def get_theoretical_mass(peptide: str):
-    """
-    Calculates the theoretical mass of a peptide
-
-    Example
-    -------
-    >>> get_theoretical_mass("MYPEPTIDE")
-    1093.4637787
+    """Calculates the theoretical mass of a peptide
+    
+    Examples:
+        >>> get_theoretical_mass("MYPEPTIDE")
+        1093.4637787
     """
     aas = peptide_parser(peptide)
     out = sum([constants.MOD_AA_MASSES[a] for a in aas])
@@ -144,39 +186,39 @@ def get_theoretical_mass(peptide: str):
 
 
 def get_precursor_mz(peptide: str, charge: int):
-    """
-    Calculates the theoretical mass/charge of a precursor peptide
+    """Calculates the theoretical mass/charge of a precursor peptide
     (assumes positive mode)
+    
+    Args:
+      peptide (str): Peptide string
+      charge (int): charge of the peptide (positive integet)
 
-    Example
-    -------
-    >>> get_precursor_mz("MYPEPTIDE", 1)
-    1094.471055167
-    >>> get_precursor_mz("MYPEPTIDE", 2)
-    547.739165817
+    Examples:
+        >>> get_precursor_mz("MYPEPTIDE", 1)
+        1094.471055167
+        >>> get_precursor_mz("MYPEPTIDE", 2)
+        547.739165817
     """
-    return get_mz(get_theoretical_mass(peptide), 0, charge)
+    return _get_mz(get_theoretical_mass(peptide), 0, charge)
 
 
-def get_forward_backward(peptide: str) -> Tuple[ndarray, ndarray]:
-    """
-    Calculates masses forward and backwards from aminoacid
+def _get_forward_backward(peptide: str) -> Tuple[ndarray, ndarray]:
+    """Calculates masses forward and backwards from aminoacid
     sequences
-
-    Examples
-    ========
-    >>> get_forward_backward("AMC")
-    (array([  1.00782503,  72.04493903, 203.08542403, 363.11607276,
-           380.11881242]), array([ 17.00273967, 177.03338839, 308.07387339, 379.11098739,
-           380.11881242]))
-    >>> get_forward_backward("AM[147]C")
-    (array([  1.00782503,  72.04493903, 219.08033403, 379.11098276,
-           396.11372242]), array([ 17.00273967, 177.03338839, 324.06878339, 395.10589739,
-           396.11372242]))
-    >>> get_forward_backward("n[+42]AM[147]C")
-    (array([ 43.01839004, 114.05550404, 261.09089904, 421.12154776,
-           438.12428742]), array([ 17.00273967, 177.03338839, 324.06878339, 395.10589739,
-           438.12428742]))
+    
+    Examples:
+        >>> _get_forward_backward("AMC")
+        (array([  1.00782503,  72.04493903, 203.08542403, 363.11607276,
+            380.11881242]), array([ 17.00273967, 177.03338839, 308.07387339, 379.11098739,
+            380.11881242]))
+        >>> _get_forward_backward("AM[147]C")
+        (array([  1.00782503,  72.04493903, 219.08033403, 379.11098276,
+            396.11372242]), array([ 17.00273967, 177.03338839, 324.06878339, 395.10589739,
+            396.11372242]))
+        >>> _get_forward_backward("n[+42]AM[147]C")
+        (array([ 43.01839004, 114.05550404, 261.09089904, 421.12154776,
+            438.12428742]), array([ 17.00273967, 177.03338839, 324.06878339, 395.10589739,
+            438.12428742]))
     """
     amino_acids = peptide_parser(peptide)
     masses = [constants.MOD_AA_MASSES[a] for a in amino_acids]
@@ -185,32 +227,47 @@ def get_forward_backward(peptide: str) -> Tuple[ndarray, ndarray]:
     return forward, backward
 
 
-def get_mz(sum_: float64, ion_offset: float, charge: int) -> float64:
+def _get_mz(sum_: float64, ion_offset: float, charge: int) -> float64:
+    """Calculates the m/z of an ion getting the offset by the type of ion and the charge,
+    Meant for internal use
+    """
     return (sum_ + ion_offset + charge * constants.PROTON) / charge
 
 
-def get_mzs(cumsum: ndarray, ion_type: str, z: int) -> List[float64]:
+def _get_mzs(cumsum: ndarray, ion_type: str, z: int) -> List[float64]:
+    """
+    Gets the m/z values from a series after being provided with the cumulative sums of the
+    aminoacids in its series, meant for internal use
+    """
+
     # return (cumsum[:-1] + constants.ION_OFFSET[ion_type] + (z * constants.PROTON))/z
     # TODO this can be vectorized if needed
-    return [get_mz(s, constants.ION_OFFSET[ion_type], z) for s in cumsum[:-2]][1:]
+    return [_get_mz(s, constants.ION_OFFSET[ion_type], z) for s in cumsum[:-2]][1:]
 
 
-def get_annotation(
+def _get_annotation(
     forward: ndarray, backward: ndarray, charge: int, ion_types: str
 ) -> OrderedDict:
-    """
-    Calculates the ion annotations based on the forward
+    """Calculates the ion annotations based on the forward
     and backward cumulative masses
+    
 
-    Example
-    =======
-    >>> fw, bw  = get_forward_backward("AMC")
-    >>> fw
-    array([  1.00782503,  72.04493903, 203.08542403, 363.11607276, 380.11881242])
-    >>> bw
-    array([ 17.00273967, 177.03338839, 308.07387339, 379.11098739, 380.11881242])
-    >>> get_annotation(fw, bw, 3, "y")
-    OrderedDict([('y1', 60.354347608199994), ...])
+    Args:
+      forward (ndarray): Forward cumulative mass
+      backward (ndarray): Backwards cumulative mass
+      charge (int): charge of the series to use
+      ion_types (str): Ion times to calculate
+
+    Returns:
+
+    Example:
+        >>> fw, bw  = _get_forward_backward("AMC")
+        >>> fw
+        array([  1.00782503,  72.04493903, 203.08542403, 363.11607276, 380.11881242])
+        >>> bw
+        array([ 17.00273967, 177.03338839, 308.07387339, 379.11098739, 380.11881242])
+        >>> _get_annotation(fw, bw, 3, "y")
+        OrderedDict([('y1', 60.354347608199994), ...])
     """
     tmp = "{}{}"
     tmp_nl = "{}{}-{}"
@@ -222,12 +279,12 @@ def get_annotation(
             cummass = backward
         else:
             raise ValueError("unknown ion_type: {}".format(ion_type))
-        masses = get_mzs(cummass, ion_type, charge)
+        masses = _get_mzs(cummass, ion_type, charge)
         d = {tmp.format(ion_type, i + 1): m for i, m in enumerate(masses)}
         all_.update(d)
         """
         for nl, offset in constants.NEUTRAL_LOSS.items():
-            nl_masses = get_mzs(cummass - offset, ion_type, charge)
+            nl_masses = _get_mzs(cummass - offset, ion_type, charge)
             d = {tmp_nl.format(ion_type, i + 1, nl): m for i, m in enumerate(nl_masses)}
             all_.update(d)
         """
@@ -238,19 +295,20 @@ def get_peptide_ions(aa_seq: str) -> Dict[str, float64]:
     """Gets the theoretical masses of fragment ions
 
     Args:
-        aa_seq (str): Aminoacid sequence with modifications
+      aa_seq (str): Aminoacid sequence with modifications
 
     Returns:
-        Dict[str, float64]: Keys are ion names and values are the mass
+      Dict[str, float64]: Keys are ion names and values are the mass
+      Examples: 
 
     Examples:
-    >>> foo = get_peptide_ions("AA")
-    >>> sorted(foo.keys())
-    ['z1b1', 'z1y1', 'z2b1', 'z2y1', 'z3b1', 'z3y1']
-    >>> foo['z1y1'] # ground truth from http://db.systemsbiology.net:8080/proteomicsToolkit/FragIonServlet.html
-    90.054955167
-    >>> foo['z1b1']
-    72.044390467
+        >>> foo = get_peptide_ions("AA")
+        >>> sorted(foo.keys())
+        ['z1b1', 'z1y1', 'z2b1', 'z2y1', 'z3b1', 'z3y1']
+        >>> foo['z1y1'] # ground truth from http://db.systemsbiology.net:8080/proteomicsToolkit/FragIonServlet.html
+        90.054955167
+        >>> foo['z1b1']
+        72.044390467
     """
     out = _get_peptide_ions(
         aa_seq,
@@ -265,20 +323,26 @@ def _get_peptide_ions(
     charges: Union[List[int], range] = range(1, 5),
     ion_types: Union[str, List[str]] = "yb",
 ) -> Dict[str, float64]:
-    """
+    """Gets a dictionary of theoretical ion masses for a peptide
 
-    Examples
-    ========
-    >>> foo = _get_peptide_ions("AA", [1,2])
-    >>> foo
-    {'z1y1': 90.054955167, ...}
+    Args:
+      aa_seq: (str):
+      charges: (Union[List[int], range]): Range of charges to use (Default value = range(1,5)
+      ion_types: Union[str, List[str]]: Ion types to calculate (Default value = "yb")
+
+    Returns:
+
+    Examples:
+        >>> foo = _get_peptide_ions("AA", [1,2])
+        >>> foo
+        {'z1y1': 90.054955167, ...}
     """
-    fw, bw = get_forward_backward(aa_seq)
+    fw, bw = _get_forward_backward(aa_seq)
     out = {}
 
     for charge in charges:
         for ion in ion_types:
-            ion_dict = get_annotation(fw, bw, charge, ion)
+            ion_dict = _get_annotation(fw, bw, charge, ion)
             ion_dict = {"z" + str(charge) + k: v for k, v in ion_dict.items()}
             out.update(ion_dict)
 
@@ -286,8 +350,19 @@ def _get_peptide_ions(
 
 
 def get_tolerance(
-    theoretical: float64, tolerance: int = 25, unit: str = "ppm"
+    theoretical: float64, tolerance: Union[float,int] = 25.0, unit: str = "ppm"
 ) -> float64:
+    """Calculates the toleranc in daltons from either a dalton tolerance or a ppm tolerance
+
+    Args:
+      theoretical (float64): Theoretical m/z to be used (only used for ppm)
+      tolerance (Union[float,int]): Tolerance value to be used (Default value = 25)
+      unit (str): Lietrally da for daltons or ppm for ... ppm (Default value = "ppm")
+
+    Returns:
+        float, the tolerance value in daltons
+
+    """
     if unit == "ppm":
         return theoretical * float(tolerance) / 10 ** 6
     elif unit == "da":
@@ -299,32 +374,22 @@ def get_tolerance(
 def is_in_tolerance(
     theoretical: float64, observed: float, tolerance: int = 25, unit: str = "ppm"
 ) -> bool_:
+    """Checks wether an observed mass is close enough to a theoretical mass
+
+    Args:
+      theoretical (float64): Theoretical mass
+      observed (float): Observed mass
+      tolerance (int): Tolerance (Default value = 25)
+      unit (str): Tolerance Unit (Default value = "ppm")
+
+    Returns:
+        bool, Wether the value observed is within the tolerance of the theoretical value
+
+    """
     mz_tolerance = get_tolerance(theoretical, tolerance, unit)
     lower = observed - mz_tolerance
     upper = observed + mz_tolerance
     return theoretical >= lower and theoretical <= upper
-
-
-def annotate_peaks(theoretical_peaks, mzs, intensities, tolerance=25, unit="ppm"):
-    annots = {}
-    max_delta = tolerance if unit == "da" else max(mzs) * tolerance / 1e6
-
-    raise DeprecationWarning(
-        "Use `annotate_peaks2`, this version of the"
-        " function is deprecated and will be removed in the future"
-    )
-
-    for mz, inten in zip(mzs, intensities):
-        matching = {
-            k: inten
-            for k, v in theoretical_peaks.items()
-            if abs(mz - v) <= max_delta and is_in_tolerance(v, mz, tolerance, unit)
-        }
-        annots.update(matching)
-
-    max_int = max([v for v in annots.values()] + [0])
-    annots = {k: v / max_int for k, v in annots.items()}
-    return annots
 
 
 def is_sorted(
@@ -336,6 +401,24 @@ def is_sorted(
     ],
     key: Callable = lambda x: x,
 ) -> bool:
+    """
+    is_sorted Checks if a list is sorted
+
+    Args:
+        lst (List): List to check if it is sorted
+        key (Callable, optional): Function to use as the key to compare. Defaults to lambdax:x.
+
+    Returns:
+        bool: Wether at least 1 element is out of order
+    
+    Examples:
+        >>> is_sorted([1,2,3,4])
+        True
+        >>> is_sorted([1,2,2,3])
+        True
+        >>> is_sorted([4,2,2,3])
+        False
+    """
     for i, el in enumerate(lst[1:]):
         if key(el) < key(lst[i]):  # i is the index of the previous element
             return False
@@ -351,17 +434,53 @@ def sort_if_needed(
     ],
     key: Callable = lambda x: x,
 ) -> None:
+    """
+    sort_if_needed Sorts a list in place if it is not already sorted
+
+    Args:
+        lst (List): List to be sorted
+        key (Callable, optional): Function to use as the key for sorting. Defaults to lambdax:x.
+
+    Examples:
+        >>> foo = [1,16,3,4]
+        >>> sort_if_needed(foo)
+        >>> foo
+        [1, 3, 4, 16]
+    """
     if not is_sorted(lst, key):
         lst.sort(key=key)
 
 
-def annotate_peaks2(
+def annotate_peaks(
     theoretical_peaks: Dict[str, float64],
     mzs: Union[List[float64], List[float]],
     intensities: Union[List[float], List[int]],
     tolerance: int = 25,
     unit: str = "ppm",
 ) -> Dict[str, float]:
+    """
+    annotate_peaks Assigns m/z peaks to annotations
+
+    Args:
+        theoretical_peaks (Dict[str, float64]): 
+            Dictionary specifying the names and masses of theoretical peaks
+        mzs (Union[List[float64], List[float]]):
+            Array of the masses to be annotated.
+        intensities (Union[List[float], List[int]]):
+            Array of the intensities that match the masses provided
+        tolerance (int, optional): 
+            Tolerance to be used to count an observed and a theoretical m/z as a match.
+            Defaults to 25.
+        unit (str, optional): 
+            The unit of the formerly specified tolerance (da or ppm).
+            Defaults to "ppm".
+
+    Returns:
+        Dict[str, float]:
+            A dictionary with the keys being the names of the ions and the values being
+            the intensities that were asigned to such ion.
+
+    """
     max_delta = tolerance if unit == "da" else max(mzs) * tolerance / 1e6
 
     mz_pairs = [[m, i] for m, i in zip(mzs, intensities)]
