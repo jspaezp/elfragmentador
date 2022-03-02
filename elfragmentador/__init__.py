b25d9fdb4794a88f25c3bb972b2f4feff33f3a24
Jspaezp <jspaezp@gmail.com>
2022-03-02 06:42:33 -0500

    changed default model

--- elfragmentador/__init__.py
+++ elfragmentador/__init__.py
@@ -6,4 +6,4 @@ except ModuleNotFoundError:
 
 __version__ = importlib_metadata.version("elfragmentador")
 
-DEFAULT_CHECKPOINT = "https://github.com/jspaezp/elfragmentador-modelzoo/raw/main/0.51.0a/0.51.0a0_Onecycle_15e_48_192_val_l%3D0.155363_epoch%3D014.ckpt"
+DEFAULT_CHECKPOINT = "https://github.com/jspaezp/elfragmentador-modelzoo/raw/main/0.51.2/0.51.2_onecycle_10e_96_120_val_l%3D0.143788_epoch%3D009.ckpt"
